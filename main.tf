2570c282305ca4b874202ed68f9521a815c0f59f
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-07-20 15:38:41 -0300

    Add EC2 instance config

--- main.tf
+++ main.tf
@@ -9,8 +9,17 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
 provider "aws" {
   region  = "us-east-1"
   profile = "bia"
 }
+
+resource "aws_instance" "bia-dev" {
+  ami = "ami-0b72821e2f351e396"
+  instance_type = "t3.micro"
+  tags = {
+    ambiente = "dev"
+    Name = "bia-dev"
+  }
+}
+
