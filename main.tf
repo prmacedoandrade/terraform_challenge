f4d6da9fea7c8e3355c64e9b6e35dcaf4404d554
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-07-28 11:43:23 -0300

    Adding user data

--- main.tf
+++ main.tf
@@ -15,12 +15,11 @@ provider "aws" {
 }
 
 resource "aws_security_group" "bia_dev" {
-  name        = "bia-dev-terraform"
-  description = "Bia dev terraform security rule"
+  name        = "bia-dev"
+  description = "access to bia-dev"
   vpc_id      = "vpc-08fbd41d57c4f621f"
 
   ingress {
-    description = "free 3001"
     from_port   = 3001
     to_port     = 3001
     protocol = "tcp"
@@ -37,9 +36,10 @@ resource "aws_security_group" "bia_dev" {
 }
 
 resource "aws_instance" "bia-dev" {
-  ami             = "ami-02f3f602d23f1659d"
+  ami             = "ami-0427090fd1714168b"
   instance_type   = "t3.micro"
   vpc_security_group_ids = [aws_security_group.bia_dev.id]
+  iam_instance_profile = aws_iam_instance_profile.role_acesso_ssm.name
 
   root_block_device {
     volume_size = 10
@@ -49,4 +49,41 @@ resource "aws_instance" "bia-dev" {
     ambiente = "dev"
     Name     = var.instance_name
   }
+
+  user_data = <<EOF
+#!/bin/bash
+
+#Instalar Docker e Git
+sudo yum update -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker ssm-user
+id ec2-user ssm-user
+sudo newgrp docker
+
+#Ativar docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+#Instalar docker compose 2
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+
+#Adicionar swap
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+
+
+#Instalar node e npm
+curl -fsSL https://rpm.nodesource.com/setup_21.x | sudo bash -
+sudo yum install -y nodejs
+
+  EOF
+
 }
---
+++ out_iam.tf
@@ -0,0 +1,36 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "role-acesso-ssm"
+resource "aws_iam_instance_profile" "role_acesso_ssm" {
+  name        = "role-acesso-ssm"
+  name_prefix = null
+  path        = "/"
+  role        = aws_iam_role.role_acesso_ssm.name
+  tags        = {}
+  tags_all    = {}
+}
+
+# __generated__ by Terraform from "role-acesso-ssm"
+resource "aws_iam_role" "role_acesso_ssm" {
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  description           = null
+  force_detach_policies = false
+  managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", "arn:aws:iam::aws:policy/AmazonECS_FullAccess", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  max_session_duration  = 3600
+  name                  = "role-acesso-ssm"
+  name_prefix           = null
+  path                  = "/"
+  permissions_boundary  = null
+  tags                  = {}
+  tags_all              = {}
+}
