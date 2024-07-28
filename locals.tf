d99ee77f5b48fe9b03e826fea48ddcfcdb10710b
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-07-28 12:12:28 -0300

    Add local and userdata

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals{
+    vpc_id = "vpc-08fbd41d57c4f621f"
+}
--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_security_group" "bia_dev" {
   name        = "bia-dev"
   description = "access to bia-dev"
-  vpc_id      = "vpc-08fbd41d57c4f621f"
+  vpc_id      = local.vpc_id
 
   ingress {
     from_port   = 3001
@@ -50,40 +50,6 @@ resource "aws_instance" "bia-dev" {
     Name     = var.instance_name
   }
 
-  user_data = <<EOF
-#!/bin/bash
-
-#Instalar Docker e Git
-sudo yum update -y
-sudo yum install git -y
-sudo yum install docker -y
-sudo usermod -a -G docker ec2-user
-sudo usermod -a -G docker ssm-user
-id ec2-user ssm-user
-sudo newgrp docker
-
-#Ativar docker
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-
-#Instalar docker compose 2
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
-
-#Adicionar swap
-sudo dd if=/dev/zero of=/swapfile bs=128M count=32
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-
-
-#Instalar node e npm
-curl -fsSL https://rpm.nodesource.com/setup_21.x | sudo bash -
-sudo yum install -y nodejs
-
-  EOF
+  user_data = "${file("userdata_biadev.sh")}"
 
 }
